c4cbe3a5fd48e9b89816696d7fc8aaf4a89d2041
kamanivenkat <venkat16042020@gmail.com>
2024-01-31 01:28:15 +0530

    TextView, EditText, Button, clickListnerEvent

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val greetingTextView = findViewById<TextView>(R.id.nnnn)
+        val inputField = findViewById<EditText>(R.id.tvName)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            greetingTextView.text = "Hello  "+ inputField.text.toString()
+        }
+
     }
 }
