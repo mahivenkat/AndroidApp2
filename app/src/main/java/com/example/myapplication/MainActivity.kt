80eeb7f959127c163f3891b27a681c7fc94c1b2c
kamanivenkat <venkat16042020@gmail.com>
2024-02-02 00:23:20 +0530

    multiple screens handling; hiding elements

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -14,17 +17,26 @@ class MainActivity : AppCompatActivity() {
         val greetingTextView = findViewById<TextView>(R.id.nnnn)
         val inputField = findViewById<EditText>(R.id.tvName)
         val button = findViewById<Button>(R.id.button)
+        val viewOffersBtn = findViewById<Button>(R.id.viewOffersBtn);
         button.setOnClickListener {
-            if (inputField.text.toString() == "")
+            val value1 = inputField.text.toString()
+            if ( value1 == "")
             {
+                viewOffersBtn.visibility = INVISIBLE
                 Toast.makeText(applicationContext,
                     "Enter the name",
                     Toast.LENGTH_LONG).
                 show()
                 greetingTextView.text = ""
             }else {
-                greetingTextView.text = "Hello  " + inputField.text.toString()
-                inputField.text.clear()
+                greetingTextView.text = "Hello  $value1"
+//                inputField.text.clear()
+                viewOffersBtn.visibility = VISIBLE
+            }
+
+            viewOffersBtn.setOnClickListener {
+                val intent = Intent(this, SecondActivity::class.java)
+                startActivity(intent)
             }
         }
 
